057c6ee6d3675b9b26739efab0d86d23ff1e1c2e
Nikhil Raghavendra <raghavendranikhil@gmail.com>
2017-09-08 22:42:53 +0800

    Some changes

--- README.md
+++ README.md
@@ -1,27 +1,22 @@
-# scratchNN
+# ScratchNN
 
-
-scracthNN is a neural network implemented using Numpy, from scratch. We train the model to map the inputs and outputs of the Exclusive-NOR gate. Line by line explaination of the code can be found below.
+ScratchNN is a neural network implemented using Numpy, from scratch. We train the model to map the inputs and outputs of the Exclusive-NOR gate. Line by line explaination of the code can be found below.
 
 ## Implementation
 
-Before we type our code, let's get our training data. For this model, we will only be having 4 instances of training data. 
+Before we define our model, let's take a look at our training data. For this model, we will only be having 4 rows of training data. The training data for our model is just four lines of the XNOR gate's truth table. 
 
-| A  | B  | C  | Output |
-|:--:|:--:|:--:|:------:|
-|0   |0   |1   |0       |
-|0   |1   |1   |1       |
-|1   |0   |1   |1       |
-|1   |1   |1   |0       |
+| A  | B  | Output |
+|:--:|:--:|:------:|
+|0   |0   |0       |
+|0   |1   |1       |
+|1   |0   |1       |
+|1   |1   |0       |
 
-The actual truth for the XNOR gate is longer but we will only be training using 4 examples. So now let's import `numpy as np` and get started.
+Now let's look at a diagrammatic representation of our model.
 
-```python
-import numpy as np
+<div align="center">
+  <img src="https://cldup.com/zJzDxXKT-z.png"><br><br>
+</div>
 
-
-def sigmoid(x, deriv=False):
-    if deriv == True:
-        return x * (1 - x)
-    return 1 / (1 + np.exp(-x))
-```
+From our table, we know that we have 2 inputs: A and B that are being mapped to an output. So we define 2 input nodes and an output node with a hidden layer consisting of 4 hidden nodes in the middle. Now let's implement this model with the help of Numpy.
--- nn.py
+++ nn.py
@@ -1,65 +1,39 @@
 import numpy as np
 
+# Training data
+X = np.array([[0, 0],
+              [0, 1],
+              [1, 0],
+              [1, 1]])
 
-def sigmoid(x, deriv=False):
-    '''
-    Sigmoid is a type of non-linear function  that
-    maps any value to a value between 0 and 1.  If
-    the deriv=True flag is passed on, the function
-    would instead calculate  the derivative of the
-    function which is used in the error back prop.
-    '''
-    if deriv == True:
-        return x * (1 - x)
-    return 1 / (1 + np.exp(-x))
-
-
-# Input data
-# 4 training examples with 3 input nodes
-X = np.array([[0, 0, 1],
-              [0, 1, 1],
-              [1, 0, 1],
-              [1, 1, 1]])
-
-# Output data
 y = np.array([[0],
               [1],
               [1],
               [0]])
 
-# Seed set so that it will return the same random numbers each time
-np.random.seed(1)
+# Weights
+w_1 = np.random.uniform(size=(2, 4))
+w_2 = np.random.uniform(size=(4, 1))
+# Bias
+bias = np.random.uniform(size=(4, 4))
 
-# Initialise weights to random values
-# 3x4 matrix of weights - 3 input nodes and 4 hidden nodes
-syn0 = 2 * np.random.random((3, 4)) - 1
-# 4x1 matrix of weights - 4 hidden nodes and 1 output node
-syn1 = 2 * np.random.random((4, 1)) - 1
 
-# Training loop
-for i in range(100000):
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+for epoch in range(10000):
     # Layers
-    l0 = X
-    l1 = sigmoid(np.dot(l0, syn0))
-    l2 = sigmoid(np.dot(11, syn1))
+    l1 = np.add(np.dot(X, w_1), bias)
+    l1 = sigmoid(l1)
+    l2 = np.dot(l1, w_2)
+    l2 = sigmoid(l2)
 
-    # Error rate
-    l2_error = y - l2
+    # Loss
+    error = np.subtract(y, l2)
+    loss = np.sum(np.square(error))
+    deriv = -2 * np.sum(error)
 
-    # Output error rate for every 10,000 epoch
-    if (i % 10000 == 0):
-        print("Error {}".format(str(np.mean(np.abs(l2_error)))))
+    # TODO: Optimize
 
-    # Error deltas
-    l2_delta = l2_error * sigmoid(l2, deriv=True)
-    l1_error = np.dot(l2_delta, syn1.T)
-    l1_delta = l1_error * sigmoid(l1, deriv=True)
-
-    # Update weights
-    syn0 += np.dot(l0.T, l1_delta)
-    syn1 += np.dot(l1.T, l2_delta)
-
-
-# Print output
-print("Output after training")
-print(l2)
+print(deriv)
