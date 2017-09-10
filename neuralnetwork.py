daa2792ed8f2c4e1163dfbb63faa06a1ae658b2d
Nikhil Raghavendra <raghavendranikhil@gmail.com>
2017-09-10 10:13:47 +0800

    Some lines of code were simplified

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -26,9 +26,7 @@ class NeuralNetwork():
     def sigmoid_derivative(self, x):
         return np.subtract(x, np.square(x))
 
-    def train_network(self, inputs, outputs, epochs):
-        inpt = inputs
-        outpt = outputs
+    def train_network(self, inpt, output, epochs):
         # Training loop
         for epoch in range(epochs):
             # Layers
@@ -36,7 +34,7 @@ class NeuralNetwork():
             l2 = self.sigmoid(np.dot(l1, self.w_2))
 
             # Error
-            l2_error = outpt - l2
+            l2_error = output - l2
             l2_delta = l2_error * self.sigmoid_derivative(l2)
             l1_error = np.dot(l2_delta, self.w_2.T)
             l1_delta = l1_error * self.sigmoid_derivative(l1)
@@ -52,13 +50,12 @@ class NeuralNetwork():
         weights = np.array([self.w_1, self.w_2])
         np.save('model.npy', weights)
 
-    def run_saved_model(self, input):
+    def run_saved_model(self, x):
         weights = np.load('model.npy')
         # Load weights
-        w_1 = weights[0]
-        w_2 = weights[1]
+        w_1, w_2 = weights[0], weights[1]
         # Layers
-        l1 = self.sigmoid(np.dot(input, w_1))
+        l1 = self.sigmoid(np.dot(x, w_1))
         l2 = self.sigmoid(np.dot(l1, w_2))
         # Return output
         return l2
